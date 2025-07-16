dba71f3b706ccbb392d0ab1e8431c6186407e2ab
Enis Gjini <egjini17@gmail.com>
2025-07-16 23:20:15 +0200

    Update index.php content and enhance CSS styles for improved typography and layout

--- index.php
+++ index.php
@@ -33,11 +33,11 @@
   <?php include 'partials/navbar.php'; ?>
   <!-- Header Start -->
   <div class="container-fluid bg-dark p-0 mb-5">
-    <div class="row g-0 flex-column-reverse flex-lg-row">
+    <div class="row g-0">
       <div class="col-lg-6 p-0 wow fadeIn" data-wow-delay="0.1s">
-        <div class="header-bg h-100 d-flex flex-column justify-content-center p-5">
+        <div class="bg-dark h-100 d-flex flex-column justify-content-center p-5">
           <h1 class="display-4 text-light mb-5">
-            Genießen Sie einen wunderbaren Tag mit Ihrer Familie
+            TF Schwimmbadbau – weil Ihr Garten mehr verdient.
           </h1>
           <div class="d-flex align-items-center pt-4 animated slideInDown">
             <a href="" class="btn btn-primary py-sm-3 px-3 px-sm-5 me-5">Mehr lesen</a>
@@ -49,17 +49,7 @@
         </div>
       </div>
       <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-        <div class="owl-carousel header-carousel">
-          <div class="owl-carousel-item">
-            <img class="img-fluid" src="img/carousel-1.jpg" alt="" />
-          </div>
-          <div class="owl-carousel-item">
-            <img class="img-fluid" src="img/carousel-2.jpg" alt="" />
-          </div>
-          <div class="owl-carousel-item">
-            <img class="img-fluid" src="img/carousel-3.jpg" alt="" />
-          </div>
-        </div>
+        <img class="img-fluid" src="img/carousel-1.jpg" alt="" />
       </div>
     </div>
   </div>
